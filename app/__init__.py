065a3a68e44573d6860625d8271a656cbcd01057
jose.correia <jose.meneses.correia@unbabel.com>
2019-08-11 02:39:38 +0100

    changed cssrf engine

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 import os
 import flask as f
 from config import config
-from flask_seasurf import SeaSurf
 from app.bot import bp_surf_bot
+from flask_wtf.csrf import CSRFProtect
 
-
-csrf = SeaSurf()
-
+csrf = CSRFProtect()
 
 def create_app(config_name):
     env_config = config[config_name]
@@ -15,9 +13,10 @@ def create_app(config_name):
     app = f.Flask(__name__)
     app.config.from_object(env_config)
     app.url_map.strict_slashes = False
-
+        
     csrf.init_app(app)
 
     app.register_blueprint(bp_surf_bot)
+    csrf.exempt(bp_surf_bot)
 
     return app
