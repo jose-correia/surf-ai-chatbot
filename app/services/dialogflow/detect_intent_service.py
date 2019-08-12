8a7b9dc6e0ed14086a7c42e979366e5efc010c94
jose.correia <jose.meneses.correia@unbabel.com>
2019-08-12 12:13:53 +0100

    created service to detect a message intent using Dialogflow

---
+++ app/services/dialogflow/detect_intent_service.py
@@ -0,0 +1,45 @@
+import dialogflow_v2 as dialogflow
+from flask import current_app
+import os
+
+class DetectIntentService():
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation.
+
+    Returns:
+
+    """
+
+    def __init__(self, session_id, text, language_code):
+        self.text = text
+        self.language_code = language_code
+        self.session_id = session_id
+        self.project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+
+    def call(self) -> bool:
+        
+        session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(self.project_id, self.session_id)
+
+        text_input = dialogflow.types.TextInput(
+            text=self.text, language_code=self.language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+
+        return response.query_result
+    
